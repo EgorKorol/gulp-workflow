cbb3bf799d17ab51ed19dbbab4b42096ee484519
Egor K <egor.k@smartum.pro>
2019-02-10 15:09:33 +0300

    Added registration sw

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './js/module';
+
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js');
+  });
+}
